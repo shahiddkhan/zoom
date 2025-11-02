7962aa004530c0984ef5bbb96725969dd8a916fe
Shahid Khan <shahidkhangit@email.com>
2025-11-02 15:11:17 +0530

    Updated backend and frontend latest code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config"; // loads .env automatically
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -16,4 +17,5 @@ const PORT = process.env.PORT || 5001;
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
