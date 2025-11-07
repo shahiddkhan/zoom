7cd7025625434827e111ff77f7ce09248fc94990
Shahid Khan <shahidkhangit@email.com>
2025-11-07 16:26:07 +0530

    Updated signup route and server configuration

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,15 +1,21 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
+import {
+  acceptFriendRequest,
+  getMyFriends,
+  getRecommendedUsers,
+  sendFriendRequest,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-//apply auth middleware to all routes
+// apply auth middleware to all routes
 router.use(protectRoute);
 
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 
 export default router;
