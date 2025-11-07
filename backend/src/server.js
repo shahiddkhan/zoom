eea463abc7b98f9121c6e07eaeeb5b8fbd9d7402
Shahid Khan <shahidkhangit@email.com>
2025-11-07 14:24:16 +0530

    Updated signup route and server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import "dotenv/config"; // loads .env automatically
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -12,7 +15,7 @@ app.use(cookieParser()); //
 
 // Use routes
 app.use("/api/auth", authRoutes);
-// app.use(express.json()); //video one 
+app.use("/api/users", userRoutes);
 
 // Set PORT from .env or fallback
 const PORT = process.env.PORT || 5001;
