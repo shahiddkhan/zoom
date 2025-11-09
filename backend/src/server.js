da869892dd0947584e81b2de6f17363d0d462786
Shahid Khan <shahidkhangit@email.com>
2025-11-09 15:40:47 +0530

    Updated signup route and server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config"; // loads .env automatically
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
@@ -11,7 +11,13 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 
 // Middleware (optional)
-app.use(express.json()); // ai one 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, //allow frontend to send the cookies
+  })
+);
+app.use(express.json()); // ai one
 app.use(cookieParser()); //
 
 // Use routes
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,25 @@ import NotificationsPage from "./pages/NotificationsPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 
 const App = () => {
+  //tanstack query
+
+  const { data,isLoading,error } = useQuery({
+    queryKey:["todos"],
+
+    queryFn: async() => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false, // auth check
+  });
+
+  console.log(data);
+
   return (
     <div className="h-screen" data-theme="night">
       <Routes>
